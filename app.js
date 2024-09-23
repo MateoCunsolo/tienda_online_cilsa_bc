01310a7e9b60f8603fbda42d7296c2068ed7a271
Mateo Cunsolo <cunsolo73@gmail.com>
2024-09-23 11:11:43 -0300

    feature: productos cargados desde un json, dinamicamente

--- app.js
+++ app.js
@@ -5,145 +5,107 @@ let cloth = document.querySelector("#cloth");
 let profile = document.querySelector("#profile");
 let supplements = document.querySelector("#supplements");
 
-const ruta = window.location.pathname; 
+const ruta = window.location.pathname;
 // Obtener la ruta del archivo HTML actual
-if (ruta.includes('contact')) { 
-    
+if (ruta.includes('contact')) {
+
     contact.style.color = "black";
     contact.style.fontWeight = "bold";
-}else if (ruta.includes('accs')) {
-    
+} else if (ruta.includes('accs')) {
+
     accs.style.color = "black";
     accs.style.fontWeight = "bold";
-}else if (ruta.includes('cloth')) {
-    
+} else if (ruta.includes('cloth')) {
+
     cloth.style.color = "black";
     cloth.style.fontWeight = "bold";
-}else if (ruta.includes('profile')){
-    
+} else if (ruta.includes('profile')) {
+
     profile.style.color = "black";
     profile.style.fontWeight = "bold";
-}else if (ruta.includes('supplements')){
-    
+} else if (ruta.includes('supplements')) {
+
     supplements.style.color = "black";
     supplements.style.fontWeight = "bold";
 }
 
 
-document.getElementById('registrationForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const name = document.getElementById('name').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const dob = document.getElementById('dob').value;
-    const text = document.getElementById('text').value.trim();
-
-    let valid = true;
-
-    // resetear errores 
-    document.getElementById('nameError').textContent = '';
-    document.getElementById('emailError').textContent = '';
-    document.getElementById('dobError').textContent = '';
-    document.getElementById('textError').textContent = '';
 
 
+// cloths
+fetch('items.json')
+    .then(response => response.json()) // Convertir la respuesta a JSON
+    .then(data => {
+        console.log(data);
+        const cloths = data.cloths;
+        const container = document.querySelector('.cloths');
 
-    // validaciones de todos los campos
-    if (name === '') {
-        valid = false;
-        document.getElementById('nameError').textContent = 'Name is required';
-    }
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (email === '' || !emailRegex.test(email)) {
-        valid = false;
-        document.getElementById('emailError').textContent = 'Please enter a valid email';
-    }
-
-    if (dob === '') {
-        valid = false;
-        document.getElementById('dobError').textContent = 'Date of birth is required';
-    } else {
-        const birthDate = new Date(dob);
-        const today = new Date();
-        const age = today.getFullYear() - birthDate.getFullYear();
-        const monthDifference = today.getMonth() - birthDate.getMonth();
-        const dayDifference = today.getDate() - birthDate.getDate();
-
-        if (age < 13 || (age === 12 && (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)))) {
-            valid = false;
-            document.getElementById('dobError').textContent = 'You must be at least 13 years old';
-        }
-    }
-
-    if( text === '') {
-        valid = false;
-        document.getElementById('textError').textContent = 'Please enter a message';
-    }else if(text.length > 500){
-        valid = false;
-        document.getElementById('textError').textContent = 'The message must be less than 500 characters';
-    }else if(text.length < 10){
-        valid = false;
-        document.getElementById('textError').textContent = 'The message must be more than 10 characters';
-    }
-
-    //si pasa todas las validaciones carga el formulario
-    if (valid) {
-        alert('Form submitted successfully!');
-    }
-
-});
-
-
-
-//aumenta el contrsate de la pagina
-let clickeado = 0;
-document.querySelector('.contrast').addEventListener('click', function(event) {
-    clickeado = !clickeado
-    let navbar = document.querySelector('#navbar-bk');
-    let body = document.querySelector('body');
-    let btn = document.querySelector('#btn');
-    let a = document.querySelectorAll('.nav-link');
-
-
-    if(clickeado){
-
-        contact.style.color = "white";
-
-        a.forEach(element => {
-            element.classList.remove('nav-black');
+        // generar las tarjetas
+        cloths.forEach(producto => {
+            const card = `
+                <div class="col-lg-3 col-md-6 mb-4">
+                    <div class="card">        
+                        <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+                        <div class="card-body px-4">
+                            <h6 class="card-title fw-bold text-secondary">${producto.nombre}</h6>
+                            <span class="precio fs-3 fw-bold">$ ${producto.precio}</span>
+                            <p class="card-text text-black-50">${producto.cuotas}</p>
+                            <div class="cont-btn d-flex justify-content-center">
+                                <a href="${producto.link}" class="btn btn-primary m-auto fw-bold">Agregar al carrito</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `;
+            container.innerHTML += card; // Agregar la tarjeta al contenedor
         });
+    }).catch(error => console.error('Error al cargar el JSON:', error));
 
-        body.classList.add('contrast-body');
-        btn.classList.remove('btn');
-        btn.classList.remove('btn-primary');
-        btn.classList.add('contrast-btn-primary');
-        
-        navbar.style.color = 'white';
-        navbar.classList.remove('bg-light');
-        navbar.classList.remove('navbar-light');
-        mavbar.classList.add('contrast-body');
 
-      
 
-    }
-    else
-    {
+// supplements
+fetch('items.json')
+    .then(response => response.json())
+    .then(data => {
+        const supplements = data.supplements;
+        const container = document.querySelector('.supplements');
 
-        contact.style.color = "black";
 
-        body.classList.remove('contrast-body');
-        btn.classList.remove('contrast-btn-primary');
-        btn.classList.add('btn');
-        btn.classList.add('btn-primary');
-        navbar.style.color = 'black';
-        navbar.classList.add('bg-light');
-        navbar.classList.add('navbar-light');
-        navbar.classList.remove('contrast-body');
-
-        a.forEach(element => {
-            element.classList.add('nav-black');
+        supplements.forEach(producto => {
+            const card = `
+                <div class="col-sm-10 col-md-4 col-lg-3 card border-primary-subtle mt-3 ms-5" style="width: 18rem;">
+                    <img src="${producto.imagen}" class="card-img-top" alt="${producto.alt}">
+                    <div class="card-body">
+                        <p class="card-text"><strong>${producto.nombre}</strong></p>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">${producto.descripcion}</h6>
+                        <h5 class="card-title">$${producto.precio.toLocaleString()}</h5>
+                        <a href="#" class="btn btn-primary">Añadir al carrito</a>
+                    </div>
+                </div>
+                `;
+            container.innerHTML += card;
         });
-        
-    }
-    
-}); 
+    }).catch(error => console.error('Error al cargar el JSON:', error));
+
+
+// accs
+fetch('items.json')
+    .then(response => response.json())
+    .then(data => {
+        const accs = data.accs;
+        const container = document.querySelector('.accs');
+        accs.forEach(producto => {
+            const card = `
+                <div class="col-sm-10 col-md-4 col-lg-3 card border-primary-subtle mt-3 ms-5" style="width: 18rem;">
+                    <img src="${producto.imagen}" class="card-img-top" alt="${producto.alt}">
+                    <div class="card-body">
+                        <p class="card-text"><strong>${producto.nombre}</strong></p>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">${producto.descripcion}</h6>
+                        <h5 class="card-title">$${producto.precio.toLocaleString()}</h5>
+                        <a href="#" class="btn btn-primary">Añadir al carrito</a>
+                    </div>
+                </div>
+                `;
+            container.innerHTML += card;
+        });
+    }).catch(error => console.error('Error al cargar el JSON:', error));
